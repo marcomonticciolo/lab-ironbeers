5549b486de7bb351f98a42f08db32c92ba646017
marco monticciolo <marcomonticciolo@marcos-MBP.attlocal.net>
2022-11-09 16:53:22 -0500

    solved lab

--- app.js
+++ app.js
@@ -3,20 +3,44 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
-// Register the location for handlebars partials here:
 
-// ...
+app.get('/beers', (req, res, next) => {
+    
+punkAPI
+  .getBeers()
+  .then(beersFromApi => {
 
-// Add the route handlers here:
+    console.log(beersFromApi)
+
+    res.render("beers.hbs", { beersFromApi })
+  })
+  .catch(error => console.log(error));
+
+
+});
+
+app.get('/random-beer', (req, res, next) => {
+
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log({responseFromAPI})
+    res.render("random-beer", responseFromAPI[0])
+  })
+  .catch(error => console.log(error));
+
+});
+
 
 app.get('/', (req, res) => {
   res.render('index');
